3ccd5b6e45beaa7f27ac5edda24fa861439bd87a
sbx_user1051 <sbx_user1051@169.254.38.31>
2025-05-10 06:42:39 +0000

    Sat May 10 06:42:39 UTC 2025

--- src/BeFaster.App/Solutions/SUM/SumSolution.cs
+++ src/BeFaster.App/Solutions/SUM/SumSolution.cs
@@ -6,7 +6,8 @@ namespace BeFaster.App.Solutions.SUM
     {
         public int Sum(int x, int y)
         {
-            throw new SolutionNotImplementedException();
+            return x + y;
         }
     }
 }
+
