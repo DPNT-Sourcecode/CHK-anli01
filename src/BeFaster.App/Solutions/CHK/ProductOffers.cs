b3d1538d1a7d1e2f197b05d76d06da32f6f734e6
sbx_user1051 <sbx_user1051@169.254.38.31>
2025-05-10 08:03:16 +0000

    Sat May 10 08:03:16 UTC 2025

--- src/BeFaster.App/Solutions/CHK/ProductOffers.cs
+++ src/BeFaster.App/Solutions/CHK/ProductOffers.cs
@@ -4,7 +4,7 @@ public static class ProductOffers
 {
     public static readonly Dictionary<char, IEnumerable<Offer>> Values = new()
     {
-        {'A', new List<Offer> {new(3, 130), new Offer(5, 200) }},
-        {'B', new List<Offer> {new(2, 45)},
-    }
+        { 'A', new List<Offer> { new(3, 130), new(5, 200) } },
+        { 'B', new List<Offer> { new(2, 45) } }
+    };
 }
